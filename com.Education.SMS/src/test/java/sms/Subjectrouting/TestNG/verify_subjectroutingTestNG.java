bb5f84af2d2ca6b7e3130144b20c6fb337a1d390
Adarsh <Adarsh@DESKTOP-FHEOHAQ>
2023-05-31 17:50:45 +0530

    local

--- com.Education.SMS/src/test/java/sms/Subjectrouting/TestNG/verify_subjectroutingTestNG.java
+++ com.Education.SMS/src/test/java/sms/Subjectrouting/TestNG/verify_subjectroutingTestNG.java
@@ -25,7 +25,7 @@ import smsGenericUtils.ExcelUtility;
 import smsGenericUtils.FileUtility;
 import smsGenericUtils.JavaUtility;
 import smsGenericUtils.WebdriverUtility;
-						//local branch
+						//local branch done successfully
 public class verify_subjectroutingTestNG extends BaseClass //TC09 //code stored in repo 
 {
 	@Test
