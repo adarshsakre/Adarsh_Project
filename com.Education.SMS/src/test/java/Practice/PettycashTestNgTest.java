7af4b993c0e591de16dc85cc002a2876384d156c
Adarsh <Adarsh@DESKTOP-FHEOHAQ>
2023-06-01 11:17:32 +0530

    123456

--- com.Education.SMS/src/test/java/Practice/PettycashTestNgTest.java
+++ com.Education.SMS/src/test/java/Practice/PettycashTestNgTest.java
@@ -16,7 +16,7 @@ public class PettycashTestNgTest extends BaseClass1
 {	
 
     //june/01
-	
+	//code changed
 
 	ExcelUtility eLib = new ExcelUtility();
 	WebdriverUtility wLib = new WebdriverUtility();
